b28f5e3842e8f3d2e20055bba69c58aefcbd529a
user121422 <user121422@NY213.local>
2016-09-19 20:47:30 -0700

    plugin challenge try

--- JqueryPlugins/StickyBar/js/main.js
+++ JqueryPlugins/StickyBar/js/main.js
@@ -15,3 +15,15 @@ $('.header').on('sticky-start', function () {
 $('.header').on('sticky-end', function () {
   $('.description').html('We build apps');
 });
+
+$('.workContact').sticky({
+	topSpacing: 20
+});
+
+$('.workContact').on('sticky-start', function () {
+  $(this).append(<a href="mailto: someone@example.com" class="sticky">Email Us</a>);
+});
+
+$('.workContact').on('sticky-end', function () {
+  $(".sticky").remove();
+});
