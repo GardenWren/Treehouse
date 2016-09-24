02304b5b8a70008c0302fdd027e2ed3246f79d22
user121422 <user121422@NY213.local>
2016-09-24 05:03:50 -0700

    Added some planning notes

---
+++ InteractiveJavaScript/ToDo/js/app.js
@@ -0,0 +1,28 @@
+//Problem: lack of interactivity
+//Solition: add interactivity
+// Add a new task
+	// When the button is pressed
+	// Create new list item from #new-task
+		// Check box 
+		// Label
+		// input text
+		// button.edit
+		// button.delete
+		// each element modified and appended
+// Edit an existing task
+	//when the Edit button is pressed
+		// if class of parent is edit mode
+			//switch back from edit mode
+			// label text become input value
+		// else
+			//switch to .editMode
+			//input value becomes labels text
+		//toggle edit mode
+		
+// Delete existing task
+	// When the delete button is pressed remove the parent list item from the ul
+// Mark a task as complete
+	// when checkbox is checked add #completed-tasks
+
+// Mark a task as incomplete
+	// When the checkbox is checked add to #incomplete-tasks
