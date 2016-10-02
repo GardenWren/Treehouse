941797db5cb5c16f88bcdb7fd11d5a8cb0b8885f
user121422 <garden.paquin@gmail.com>
2016-10-02 12:28:11 -0700

    finished callback function to add photos with links from flickr to the page

--- AJAXBasics/FlickrPage/js/FlickrApp.js
+++ AJAXBasics/FlickrPage/js/FlickrApp.js
@@ -3,15 +3,18 @@ $(document).ready(function(){
 		$('button').removeClass('selected');
 		$(this).addClass('selected'); // show button as selected
 		
-		var flickrAPI ="http//api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?"; // flickr API link
+		var flickrAPI ="http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?"; // flickr API link
 		var animal = $(this).text();
 		var flickrOptions = {tags: animal, format: "json"};// send correct search to flickr
 		var displayPhotos = function (data){
 	var photoHTML = "<ul>";
 	$.each(data.items,function(i, photo){
 		photoHTML+= '<li class="grid-25 tablet-grid-50">';
-		
-	})		
+		photoHTML+= '<a href="'+photo.link+'" class="image">';
+		photoHTML+='<img src="'+photo.media.m+'"></a></li>';
+	});
+	 photoHTML+= '</ul>';
+	 $('#photos').html(photoHTML);		
 			
 		};
 		
