83dbec4a9113f14c440c6fe38bb4c6ca34fae853
user121422 <garden.paquin@gmail.com>
2016-10-02 12:09:53 -0700

    added url and request data, working on callback function

---
+++ AJAXBasics/FlickrPage/js/FlickrApp.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	$	('button').click(function(){
+		$('button').removeClass('selected');
+		$(this).addClass('selected'); // show button as selected
+		
+		var flickrAPI ="http//api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?"; // flickr API link
+		var animal = $(this).text();
+		var flickrOptions = {tags: animal, format: "json"};// send correct search to flickr
+		var displayPhotos = function (data){
+	var photoHTML = "<ul>";
+	$.each(data.items,function(i, photo){
+		photoHTML+= '<li class="grid-25 tablet-grid-50">';
+		
+	})		
+			
+		};
+		
+		$.getJSON(flickrAPI,flickrOptions, displayPhotos); // get photos from flickr
+	});// end click handler
+	
+}); // end ready
