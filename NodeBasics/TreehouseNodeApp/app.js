f088757fafa5c9e61d154ae8b1883193a1ba9e0e
user121422 <garden.paquin@gmail.com>
2016-10-23 11:48:13 -0700

    parsing errors in Treehouse Node App

--- NodeBasics/TreehouseNodeApp/app.js
+++ NodeBasics/TreehouseNodeApp/app.js
@@ -3,32 +3,48 @@
 
 //contect to api url(https://teamtreehouse.com/username.json)
 var https = require("https");
+var http = require("http");
 var username = "gardenwren";
 
+
+// Print out Message
 function printMessage(username, badgeCount, points){
     var message = username + " has " + badgeCount + " total badge(s) and " + points + " points in JavaScript";
     console.log(message);
     };
 
+// Print out Error Messages
+function printError(error){
+  console.error(error.message);	
+  }
+  
 //Read the data
-var request = https.get("https://teamtreehouse.com/" + username + ".json", function(response){
+var request = https.get("https://teamtreehouse.com/" + username + ".json", 
+  function(response){
 	
-//Read the data
-var body ="";
+    //Read the data
+    var body ="";
     response.on('data',function(chunk){
-	body+= chunk;
+	  body+= chunk;
     });
 
-response.on("end", function(){
-	//parse the data
-	var profile = JSON.parse(body);
-	//print the data out
-    printMessage(username, profile.badges.length,      profile.points.JavaScript);
+    response.on("end", function(){
+	  if (response.statusCode === 200 ){
+	
+	    //parse the data
+	    try{
+	      var profile = JSON.parse(body);
+	    }catch(error){
+          //Parse Error
+	      printError(error);
+	    };
+	    //print the data out
+        printMessage(username, profile.badges.length,      profile.points.JavaScript);
+      }else{
+	    printError({message: "There was and error getting the profile for " + username + ".  " +  http.STATUS_CODES[response.statusCode]})
+    }
+  });
 });
 
-
-  
-  });
-  request.on("error",function(error){
-  consol.error(error.message);	
-  });
+  //Connection error
+  request.on("error", printError);
